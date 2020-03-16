830f565234925ddbab8879d603c1ac7861a0244e
MasanaIto <z3heng0326@gmail.com>
2020-03-17 00:10:15 +0900

    プロフィール編集機能を実装
    editメソッド、updateメソッドを実装
    editページルート定義

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,19 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 use App\User;
+use App\Services\CheckExtensionServices;
+use App\Services\FileUploadServices;
 
 class UserController extends Controller
 {
     /**
-     * ユーザー一覧表示
+     * プロフィール表示機能
      */
     public function show(int $id)
     {
         $user = User::findorFail($id);
 
-        // dd($user);
-
         return view('users.show', compact('user'));
+    }
+
+
+
+    /**
+     * プロフィール編集機能
+     */
+    public function edit(int $id)
+    {
+        $user = User::findorFail($id);
+
+        return view('users.edit', compact('user')); 
+    }
+
+
+
+    /**
+     * プロフィール更新機能
+     */
+    public function update(Request $request, int $id)
+    {
+
+        $user = User::findorFail($id);
+
+        if(!is_null($request['img_name'])){
+            $imageFile = $request['img_name'];
+
+            $list = FileUploadServices::fileUpload($imageFile);
+            list($extension, $fileNameToStore, $fileData) = $list;
+            
+            $data_url = CheckExtensionServices::checkExtension($fileData, $extension);
+            $image = Image::make($data_url);        
+            $image->resize(400,400)->save(storage_path() . '/app/public/images/' . $fileNameToStore );
+
+            $user->img_name = $fileNameToStore;
+        }
+        
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->sex = $request->sex;
+        $user->self_introduction = $request->self_introduction;
+
+        $user->save();
+
+        return redirect()->to('home'); 
     }
 }
