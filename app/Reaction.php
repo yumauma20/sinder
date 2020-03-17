27d2c8aafde7f71784fe3c4065e0053e1da3ed5f
MasanaIto <z3heng0326@gmail.com>
2020-03-17 12:44:54 +0900

    UserとReactionを紐づけるためリレーションの設定を追加する
    User -> 多 Reaction という関係になるので UserモデルにhasManyメソッドを設定
    Reactionモデルからそれぞれ1つのidを参照するためbelongsToメソッドを設定

--- app/Reaction.php
+++ app/Reaction.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reaction extends Model
 {
-    //
+    public $incrementing = false;  // インクリメントIDを無効化
+    public $timestamps = false; // update_at, created_at を無効化
+
+    public function toUserId()
+    {
+        return $this->belongsTo('App\User', 'to_user_id', 'id');
+    }
+
+    public function fromUserId()
+    {
+        return $this->belongsTo('App\User', 'from_user_id', 'id');
+    }
 }
