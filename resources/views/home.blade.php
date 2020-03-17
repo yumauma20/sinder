60b948e9190da5b04f84f0107b8e064aa7c415a2
MasanaIto <z3heng0326@gmail.com>
2020-03-17 18:47:19 +0900

    マッチングしたユーザー一覧画面を作成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,7 @@
       <li class="personIcon">
         <a href="/users/show/{{Auth::id()}}"><i class="fas fa-user fa-2x"></i></a></li>
       <li class="appIcon"><a href="{{route('home')}}"><img src="/storage/images/techpit-match-icon.png"></a></li>
+      <li class="messageIcon"><a href="{{route('matching')}}"><i class="fas fa-2x fa-comments"></a></i></li>
     </ul>
   </nav>
   <div id="tinderslide">
